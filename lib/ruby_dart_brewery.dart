9b8776d50789dab7d86b8653dc4c667ff46aee10
Kevin Moore <github@j832.com>
2014-08-08 17:44:19 -0700

    and more

--- lib/ruby_dart_brewery.dart
+++ lib/ruby_dart_brewery.dart
@@ -83,6 +83,68 @@ Future<VersionInfo> _getPublishedVersionInfoOrNull(String channel, int revision)
   });
 }
 
+class BinaryInfo {
+  final String binaryPath;
+  final int binarySize;
+  final String md5;
+  final String sha256;
+  final VersionInfo info;
+
+  BinaryInfo(this.binaryPath, this.md5, this.sha256, this.info, this.binarySize);
+
+  static Future<BinaryInfo> get(String channel, int revision, String binaryPath) {
+    return getVersionInfo(channel, revision).then((info) {
+
+    });
+  }
+}
+
+/// requests:
+///   key: full url
+///   value: either 'stringContent' or 'size'
+///
+/// response:
+///   key: full url
+///   value - if it exists
+///   - stringContent: content of the file (String)
+///   - size: size of the file (int)
+///   value - if not found - `null`
+Future<Map<String, dynamic>> getFiles(http.Client client, Map<String, String> requests) {
+  var result = {};
+
+  return Future.forEach(requests.keys, (requestUrl) {
+    var type = requests[requestUrl];
+
+    if (type == 'stringContent') {
+      return getStringContentOrNull(client, requestUrl).then((content) {
+        result[requestUrl] = content;
+      });
+    } else if (type == 'size') {
+      return getSizeOrNull(client, requestUrl).then((size) {
+        result[requestUrl] = size;
+      });
+    } else {
+      throw 'I do not like "$type".';
+    }
+  }).then((_) {
+    return result;
+  });
+}
+
+Future<String> getStringContentOrNull(http.Client client, String url) {
+  return client.read(url).catchError((err, stack) {
+    return null;
+  });
+}
+
+Future<int> getSizeOrNull(http.Client client, String url) {
+  return client.head(url).then((response) {
+    return response.contentLength;
+  }).catchError((err, stack) {
+    return null;
+  });
+}
+
 class VersionInfo {
   final int revision;
   final String version;
